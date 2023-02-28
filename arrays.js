2aac299979021e24e1109ae5c6889c69aa507a1b
Chinika charles <chinikacharles@chinikas-mbp.home>
2023-02-28 10:32:53 +0000

    Added in different solutions for 2 questions in Objects and Arrays

--- arrays.js
+++ arrays.js
@@ -87,6 +87,7 @@ console.log(stoppingAtDurham);
  * Q9.
  * If we are stopping at Durham, print "Yay Durham!" to the terminal. If not print "Aww...".
  */
+
 if (stoppingAtDurham){
     return console.log("Yay Durham!"); 
     } else {
